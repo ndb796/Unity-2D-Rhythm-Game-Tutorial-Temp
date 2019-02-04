194054a6100ae859ad48c5963db3859ce4cff22b
ndb796 <ndb796@naver.com>
2019-02-05 01:28:17 +0900

    노트 오브젝트 풀링 처리

--- Assets/Scripts/NoteBehavior.cs
+++ Assets/Scripts/NoteBehavior.cs
@@ -9,6 +9,11 @@ public class NoteBehavior : MonoBehaviour {
     private GameManager.judges judge;
     private KeyCode keyCode;
 
+    public void Initialize()
+    {
+        judge = GameManager.judges.NONE;
+    }
+
     void Start()
     {
         if (noteType == 1) keyCode = KeyCode.D;
@@ -26,7 +31,7 @@ public class NoteBehavior : MonoBehaviour {
             // 해당 노트에 대한 판정을 진행합니다.
             Debug.Log(judge);
             // 노트가 판정 선에 닿기 시작한 이후로는 해당 노트를 제거합니다.
-            if (judge != GameManager.judges.NONE) Destroy(gameObject);
+            if (judge != GameManager.judges.NONE) gameObject.SetActive(false);
         }
     }
 
@@ -48,7 +53,7 @@ public class NoteBehavior : MonoBehaviour {
         else if (other.gameObject.tag == "Miss Line")
         {
             judge = GameManager.judges.MISS;
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
     }
 
