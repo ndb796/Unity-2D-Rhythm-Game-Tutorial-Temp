9a1438ff7ae05b1ba94156895ec56cf756cf2c94
ndb796 <ndb796@naver.com>
2019-02-08 20:29:08 +0900

    순위 목록 표 구현

--- Assets/Scripts/GameResultManager.cs
+++ Assets/Scripts/GameResultManager.cs
@@ -5,6 +5,8 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Firebase;
+using Firebase.Unity.Editor;
 
 public class GameResultManager : MonoBehaviour {
 
@@ -13,7 +15,11 @@ public class GameResultManager : MonoBehaviour {
     public Text maxComboUI;
     public Image RankUI;
 
-	void Start () {
+    public Text rank1UI;
+    public Text rank2UI;
+    public Text rank3UI;
+
+    void Start () {
         musicTitleUI.text = "" + PlayerInformation.musicTitle;
         scoreUI.text = "점수: " + (int) PlayerInformation.score;
         maxComboUI.text = "최대 콤보: " + PlayerInformation.maxCombo;
@@ -45,6 +51,47 @@ public class GameResultManager : MonoBehaviour {
         {
             RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank C");
         }
+        rank1UI.text = "데이터를 불러오는 중입니다.";
+        rank2UI.text = "데이터를 불러오는 중입니다.";
+        rank3UI.text = "데이터를 불러오는 중입니다.";
+        Firebase.Database.DatabaseReference reference;
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://unity-rhythm-game-tutorial.firebaseio.com/");
+        reference = Firebase.Database.FirebaseDatabase.DefaultInstance.GetReference("ranks")
+            .Child(PlayerInformation.selectedMusic);
+        // 데이터 셋의 모든 데이터를 JSON 형태로 가져옵니다.
+        reference.OrderByChild("score").GetValueAsync().ContinueWith(task => {
+            // 성공적으로 데이터를 가져온 경우
+            if (task.IsCompleted)
+            {
+                List<string> rankList = new List<string>();
+                List<string> emailList = new List<string>();
+                Firebase.Database.DataSnapshot snapshot = task.Result;
+                // JSON 데이터의 각 원소에 접근합니다.
+                foreach (Firebase.Database.DataSnapshot data in snapshot.Children)
+                {
+                    IDictionary rank = (IDictionary) data.Value;
+                    emailList.Add(rank["email"].ToString());
+                    rankList.Add(rank["score"].ToString());
+                }
+                // 정렬 이후 순서를 뒤집어 내림차순 정렬합니다.
+                emailList.Reverse();
+                rankList.Reverse();
+                // 최대 상위 3명의 순위를 차례대로 화면에 출력합니다.
+                rank1UI.text = "플레이 한 사용자가 없습니다.";
+                rank2UI.text = "플레이 한 사용자가 없습니다.";
+                rank3UI.text = "플레이 한 사용자가 없습니다.";
+                List<Text> textList = new List<Text>();
+                textList.Add(rank1UI);
+                textList.Add(rank2UI);
+                textList.Add(rank3UI);
+                int count = 1;
+                for (int i = 0; i < rankList.Count && i < 3; i++)
+                {
+                    textList[i].text = count + "위: " + emailList[i] + " (" + rankList[i] + ")";
+                    count = count + 1;
+                }
+            }
+        });
     }
 
     public void Replay()
